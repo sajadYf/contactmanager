6b15b24a3449b779a2159bbf941676372006d1f3
sajadYf <sajadyaft@gmail.com>
2019-10-16 16:01:00 +0330

    Updates

---
+++ precache-manifest.a3c77b4d9a924adce72c4d7b1b3538c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb11d015d01e71f4595dbca98dfed98",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "affa18096e2b99e27030",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fd608414c0cbee4e5a0d",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "affa18096e2b99e27030",
+    "url": "/contactmanager/static/js/2.a9c26e81.chunk.js"
+  },
+  {
+    "revision": "fd608414c0cbee4e5a0d",
+    "url": "/contactmanager/static/js/main.0005f6bd.chunk.js"
+  },
+  {
+    "revision": "28b54336cd64c84b0021",
+    "url": "/contactmanager/static/js/runtime-main.344dc1de.js"
+  }
+]);
